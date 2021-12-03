e92cc43641aab67ed6dd0a1971972f3744b717f9
marieprosper77 <85536042+marieprosper77@users.noreply.github.com>
2021-12-02 21:00:02 -0500

    test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Project-4
-
+Link to our site: http://cc-approval.herokuapp.com/
 
 
 ## Overview
@@ -19,3 +19,32 @@ There are times when even a seemingly manageable debt, such as credit cards, goe
 A lot of us would be able to relate to this scenario. We may have missed credit card payments once or twice because of forgotten due dates or cash flow issues. But what happens when this continues for months? How to predict if a customer will be defaulter in next months?
 
 To reduce the risk of Banks, this model has been developed to predict customer defaulter based on demographic data like gender, age, marital status and behavioral data like last payments, past transactions etc.
+
+
+
+
+## **Technologies**
+
+- Python (Jupyter Notebook, Pandas)
+- Flask 
+- SQL/ Postgres DB
+- JavaScript Libraries 
+- HTML/ CSS (Bootstrap)
+- Csv
+- Machine Learning
+
+
+
+These are the steps: 
+1. Data Cleaning
+    - Removing extraneous columns
+    - Null values
+    - Duplicates
+    - Merging datasets by grouping shows
+2. Individual scatter plots for each feature
+3. Regularization: Coefficient plots with Linear Regression
+4. Function to test the familiar Regressors
+5. Scaling the data – StandScaler & MinMaxScaler
+6. Model Selection & Prediction
+7. MSE & R-squared score: Lesson Learnt 
+8. Exported the model -> Flask implementation
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = joblib.load("CreditRiskModel.h2")
 
 @app.route('/')
 def home():
@@ -14,22 +15,36 @@ def predict():
 	'''
 	for rendering results on HTML
 	'''	
-	features = [int(x) for x in request.form.values()]
+	
+	# ImmutableMultiDict([('children', '3'), ('income', '20000'), ('income', '3'), ('gender', '1'), ('car', '0'), ('realty', '0')])
+	# print(request.form)
+
+	# print(pd.DataFrame(request.form.to_dict()))
+	data = dict((key, request.form.getlist(key)) for key in request.form.keys())
+
+	print(data)
+	# features = [int(x) for x in request.form.values()]
+	features = pd.DataFrame(data)
+	print(features)
+
+	# rename names to the names of the 
 
 	# re-arranging the list as per data set
-	feature_list = [features[4]] + features[:4] + features[5:11][::-1] + features[11:17][::-1] + features[17:][::-1]
-	features_arr = [np.array(feature_list)]
+	# feature_list = [features[4]] + features[:4] + features[5:11][::-1] + features[11:17][::-1] + features[17:][::-1]
+	# features_arr = [np.array(feature_list)]
 
-	prediction = model.predict(features_arr)
+	# prediction = model.predict(features_arr)
+	prediction = model.predict(features)
 
-	print(features_arr)
+
+	print(features)
 	print("prediction value: ", prediction)
 
 	result = ""
-	if prediction == 1:
-		result = "The credit card holder will be Defaulter in the next month"
+	if prediction == "low_risk":
+		result = "The credit card application is likely to be APPROVED*"
 	else:
-		result = "The Credit card holder will not be Defaulter in the next month"
+		result = "The credit card application is likely to be DENIED*"
 
 	return render_template('index.html', prediction_text = result)
 
